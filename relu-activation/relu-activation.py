9aff96e5ed87ea2b5498855cb0f2a0b61a5764f8
Ali Uğur Kaymaz <76847962+ugurkaymaz-hub@users.noreply.github.com>
2026-04-28 13:05:28 +0300

    Add solution for relu-activation

---
+++ relu-activation/relu-activation.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def relu(x):
+    """
+    Implement ReLU activation function.
+    """
+    # Write code here
+    x = np.asarray(x , dtype=float)
+    return np.maximum(0,x)
+  
